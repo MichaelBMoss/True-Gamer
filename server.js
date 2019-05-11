1ce620bde1884ee3c14492e1f813762ffd5ecba5
Mike Moss' Ubuntu Linux <michaelbmoss@gmail.com>
2019-05-11 15:34:37 -0700

    initializing true gamer project

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const path = require('path');
 const express = require('express');
+const axios = require('axios');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
 const app = express();
 app.use(express.json());
 
 /// YOUR ROUTES GO HERE!
-
+axios({
+  url: "https://api-v3.igdb.com/achievements",
+  method: 'POST',
+  headers: {
+      'Accept': 'application/json',
+      'user-key': "ff39cd10fb80e5920517d94fa9b2f558"
+  },
+  data: "fields achievement_icon,category,created_at,description,external_id,game,language,name,owners,owners_percentage,rank,slug,tags,updated_at;"
+})
+  .then(response => {
+      console.log(response.data);
+  })
+  .catch(err => {
+      console.error(err);
+  });
 
 /////////////////////////////////////////////
 

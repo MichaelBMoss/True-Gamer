0e046e862a9c3c4ef749ada2ef1c7c1b650dea3a
Nadica08 <nadiabc3@gmail.com>
2019-05-15 20:38:24 -0700

    reverted reviewPage back to it's original set.

--- client/src/components/PageComponents/ReviewsPage/ReviewsPage.js
+++ client/src/components/PageComponents/ReviewsPage/ReviewsPage.js
@@ -1,34 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import reviews from '/gamer_review.json';
+
 
 import './ReviewsPage.css';
 class ReviewsPage extends Component {
-    state = {
-      reviews: [
-        { 
-          id: ""
-          username: "",
-          date: "mydate1.toString()"
-          _title: "",
-          get title() {
-            return this._title;
-          },
-          set title(value) {
-            this._title = value;
-          },
-          review: "",
 
-
-        }
-      ]
-    }
   render() {
     console.log('fetching data');
     return (
       <div className="ReviewsPage">
         <header className="ReviewsPage-header">
-          <p>Reviews Page reviews={this.state.reviews}
+          <p>Reviews Page={this.state.reviews}
           </p>
 {/*          <Link to="/blog/">Blog</Link>
           <Link to="/write/">Write article</Link> */}

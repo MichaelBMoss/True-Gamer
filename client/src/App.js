6cf971cdfda6ba14641d495ebf2d30517a74fc4b
Darla Brown <darlanebrown@gmail.com>
2019-05-16 20:07:02 -0700

    Write Articles

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import MainPage from './components/PageComponents/MainPage/MainPage.js';
 import ReviewsPage from './components/PageComponents/ReviewsPage/ReviewsPage.js';
+import WriteArticle from './components/PageComponents/WriteArticle.js';
 
 class App extends Component {
   
@@ -31,15 +32,19 @@ class App extends Component {
           <h1 className="App-title">True Gamer</h1>
           <Link to="/">Home</Link>
           <Link to="/ReviewsPage/">Reviews</Link>
+          <Link to="/WriteArticlePage/">Write Reviews</Link>
         </nav>
-
+      
         <div className="App-mainContent">
           <Switch>
             <Route exact path='/' component={MainPage} />
             <Route exact path='/ReviewsPage/' component={ReviewsPage} />
+            <Route exact path='/WriteArticlePage/' component={WriteArticle} />
           </Switch>
+          </div>
         </div>
-      </div>
+      
+      
     );
   }
 }
---
+++ client/src/components/PageComponents/WriteArticle.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import './WriteArticle.css';
+
+class WriteArticle extends Component {
+  state = {
+    title: 'True Gamer',
+    text: 'true gamer',
+  }
+
+  onChangeContent = (ev) => {
+    this.setState({
+      text: ev.target.value,
+    });
+  }
+
+  onChangeTitle = (ev) => {
+    this.setState({
+      title: ev.target.value,
+    });
+  }
+  
+  
+  onChangeGame = (ev) => {
+    this.setState({
+      title: ev.target.value,
+    });
+  }
+
+
+  submit = () => {
+    const formData = {
+      title: this.state.title,
+      text: this.state.text,
+    };
+
+    fetch('/api/mongodb/blogposts/', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(formData),
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Got this back', data);
+
+        // Redirect to blog
+        this.props.history.push('/blog/');
+      });
+  }
+
+
+  render() {
+    return (
+      <div className="WriteArticle">
+        <h1>Write an article</h1>
+        <div className="flex-container">
+        <input
+            name="name"
+            placeholder="Name"
+            value={this.state.name}
+            onChange={this.onChangeName}
+          
+          />
+        <br />
+          <div classGame="flex-inner">
+        <input
+            name="game"
+            placeholder="Game"
+            value={this.state.game}
+            onChange={this.onChangeGame}
+          />
+        <br />
+        </div>
+
+        <textarea
+            name="reviews"
+            placeholder="Reviews"
+            value={this.state.details}
+            onChange={this.onChangeReview}
+          />
+          <div classContent="flex-inner">
+        </div>
+        <br />
+        
+
+        <button onClick={this.submit}>Add to blog</button>
+      </div>
+      </div>
+
+    );
+  }
+}
+
+export default WriteArticle;

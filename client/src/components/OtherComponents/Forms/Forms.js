0f4bf33a88594b6f5e07806d8875100c75404187
Mike Moss' Ubuntu Linux <michaelbmoss@gmail.com>
2019-05-22 17:08:25 -0700

    added mongodb functionality

--- client/src/components/OtherComponents/Forms/Forms.js
+++ client/src/components/OtherComponents/Forms/Forms.js
@@ -6,6 +6,11 @@ class Forms extends Component {
     FormGame: '',
     FormName: '',
     FormReview: '',
+    reviews: []
+  }
+
+  componentDidMount() {
+	  this.fetchPosts();
   }
 
   onChangeFormReview = (ev) => {
@@ -32,7 +37,8 @@ class Forms extends Component {
       FormGame: this.state.FormGame,
       FormReview: this.state.FormReview,
     };
-    fetch('/api/mongodb/blogposts/', {
+    
+    fetch('/api/mongodb/TrueGamer/', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(formData),
@@ -40,42 +46,65 @@ class Forms extends Component {
       .then(response => response.json())
       .then(data => {
         console.log('Got this back', data);
-
-        // Redirect to blog
-        this.props.history.push('/blog/');
+        this.fetchPosts();
       });
   }
 
+  fetchPosts = () => {
+    console.log('Fetching data from API');
+    fetch('/api/mongodb/TrueGamer/')
+      .then(response => response.json())
+      .then(data => {
+        console.log('Got data back', data);
+        let reverseReviews = data.reverse();
+        this.setState({
+          FormGame: '',
+          FormName: '',
+          FormReview: '',
+          reviews: reverseReviews,
+        });
+      });
+  }
 
   render() {
     return (
       <div className="Forms">
         <div className="FormsOuterBox">
           <div className="FormInnerBox">
-                        <h1>Write a Review</h1>
-                          <input
-                            className="FormNameBox"
-                            name="FormName"
-                            placeholder="Name"
-                            value={this.state.FormName}
-                            onChange={this.onChangeFormName}
-                          />
-                          <br/> <br/>
-                        <input
-                            className="FormGameBox"
-                            name="FormGame"
-                            placeholder="Game"
-                            value={this.state.FormGame}
-                            onChange={this.onChangeForm}
-                          />
-                        <textarea
-                            className="FormReviewBox"
-                            name="FormReview"
-                            placeholder="Review"
-                            value={this.state.FormReview}
-                            onChange={this.onChangeFormReview}
-                          />
-                        <button className="FormPostButton" onClick={this.submit}>Post</button>
+            <h1>Write a Review</h1>
+              <input
+                className="FormNameBox"
+                name="FormName"
+                placeholder="Name"
+                value={this.state.FormName}
+                onChange={this.onChangeFormName}
+              />
+              <br/> <br/>
+            <input
+                className="FormGameBox"
+                name="FormGame"
+                placeholder="Game"
+                value={this.state.FormGame}
+                onChange={this.onChangeFormGame}
+              />
+            <textarea
+                className="FormReviewBox"
+                name="FormReview"
+                placeholder="Review"
+                value={this.state.FormReview}
+                onChange={this.onChangeFormReview}
+              />
+            <button className="FormPostButton" onClick={this.submit}>Post</button>
+            <h1 className="ReviewsTitle">Reviews</h1>
+            <div className="ReviewsBox">
+              {this.state.reviews.map((review, index) => (
+                <div className="Review" key={index}>
+                  <p> {review.FormName} </p>
+                  <p> {review.FormGame} </p>
+                  <p> {review.FormReview} </p>
+                </div>
+            ))}
+            </div>
           </div> {/* innerbox */}
         </div> {/* outerbox */}
       </div>
